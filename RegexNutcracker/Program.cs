9b02d214674cb1e812808882050efbb1e865adca
Smith Wilson <moskalev99@mail.ru>
2017-11-16 23:31:59 +0300

    Файловый сервис.

--- RegexNutcracker/Program.cs
+++ RegexNutcracker/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RegexNutcracker.Services;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,21 @@ namespace RegexNutcracker
 {
 	public class Program
 	{
-		static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
+			var Encoded = new List<string> { "hello", ",", "world", "." };
 
+			#region Fields
+			var _path = Directory.GetCurrentDirectory();
+			var _modelFile = "model.txt";
+			var _unEncodedFile = "unEncoded.txt";
+			var _outputFile = "output.txt"; 
+			#endregion
+
+			var models = await FileService.ReadFromFile(_path, _modelFile);
+			var unEncoded = await FileService.ReadFromFile(_path, _unEncodedFile);
+
+			await FileService.WriteToFile(_path, _outputFile, Encoded);
 		}
 	}
 }
---
+++ RegexNutcracker/Services/FileService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexNutcracker.Services
+{
+	public static class FileService
+	{
+		/// <summary>
+		/// Считывание из файла.
+		/// </summary>
+		/// <param name="path">Путь.</param>
+		/// <param name="fileName">Имя файла.</param>
+		/// <returns></returns>
+		public static Task<List<string>> ReadFromFile (string path, string fileName)
+		{
+			return Task.Run(() =>
+			{
+				using (var sr = new StreamReader(path + "/" + fileName, Encoding.Default))
+				{
+					if (!sr.BaseStream.CanRead)
+					{
+						Console.WriteLine($"Ошибка чтения файла {path}/{fileName}.");
+						return new List<string>();
+					}
+
+					var input = new List<string>();
+					while (!sr.EndOfStream)
+					{
+						input.Add(sr.ReadLine());
+					}
+
+					return input;
+				}
+			});
+		}
+
+		/// <summary>
+		/// Запись в файл.
+		/// </summary>
+		/// <param name="path">путь к файлу.</param>
+		/// <param name="fileName">Имя файла.</param>
+		/// <param name="encoded">Закодированые строки.</param>
+		/// <returns></returns>
+		public static Task WriteToFile(string path, string fileName, List<string> encoded)
+		{
+			return Task.Run(() =>
+			{
+				using (var sw = new StreamWriter(path + "/" + fileName, false, Encoding.UTF8))
+				{
+					if (!sw.BaseStream.CanWrite)
+					{
+						Console.WriteLine($"Ошибка записи в файл {path}/{fileName}.");
+						return;
+					}
+
+					foreach (var line in encoded)
+					{
+						sw.WriteLine(line);
+					}
+				}
+			});
+		}
+	}
+}
