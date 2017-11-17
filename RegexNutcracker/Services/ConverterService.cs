ee31ea56eab81e04bbfbde2f733447de3509b3ec
Smith Wilson <moskalev99@mail.ru>
2017-11-17 17:28:19 +0300

    Улучшение уровня распознования.

---
+++ RegexNutcracker/Services/ConverterService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexNutcracker.Services
+{
+	public static class ConverterService
+	{
+		public static string StringToRegex(this string value, string model)
+		{
+			string pattern;
+
+			value = value
+				.TrimStart()
+				.Trim()
+				.TrimEnd();
+
+			model = model
+				.TrimStart()
+				.Trim()
+				.TrimEnd();
+
+			if (value != model)
+			{
+				pattern = $"({value})|({model})";
+			}
+			else
+			{
+				pattern = value;
+			}
+
+			// . - + №
+			var result = pattern
+				.TrimStart()
+				.Trim()
+				.TrimEnd()
+				.Insert(0, "#")
+				.Insert(pattern.Length + 1, "#")
+				.Replace("-", @"(#|\-)?")
+				.Replace("/", @"(#|\/|\-)?")
+				.Replace(" ", @"(#|\-)?")
+				.Replace(".", @"(#|\.|\-)?")
+				.Replace(",", @"(#|\,)?")
+				.Replace("+", @"(#|\+|\-)?")
+				.Replace("№", @"(#|\№)?")
+				.Replace("\"", @"(#|\"")?")
+				.Replace("*", @"(#|\*)?");
+			
+			return result;
+		}
+	}
+}
