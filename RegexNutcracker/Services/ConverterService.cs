7b839c604fc2f86db8bbadf08d6ab11fe299b34b
Smith Wilson <moskalev99@mail.ru>
2017-12-07 20:22:18 +0300

    Правка конвертора. Игнорирование простых входных данных.

--- RegexNutcracker/Services/ConverterService.cs
+++ RegexNutcracker/Services/ConverterService.cs
@@ -9,67 +9,98 @@ namespace RegexNutcracker.Services
 {
 	public static class ConverterService
 	{
-		/// <summary>
-		/// Преобразует входные данные в регулярное выражение.
-		/// Распознает . - + № , / * " пробел ( )
-		/// </summary>
-		/// <param name="value"></param>
-		/// <param name="model"></param>
-		/// <returns></returns>
-		public static string StringToRegex(this string value, string model)
-		{
-			string pattern;
+        /// <summary>
+        /// Преобразует входные данные в регулярное выражение.
+        /// Распознает . - + № , / * " пробел ( ) ®
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string StringToRegex(this string value, string model)
+        {
+            #region Fields
+            string pattern; 
+            #endregion
 
-			value = value
+
+            value = value
 				.TrimStart()
 				.Trim()
-				.TrimEnd();
+				.TrimEnd()
+                .ParseBracket();
 
 			model = model
 				.TrimStart()
 				.Trim()
-				.TrimEnd();
+				.TrimEnd()
+                .ParseBracket();
 
-			if (value != model && !String.IsNullOrWhiteSpace(value) && !String.IsNullOrWhiteSpace(model))
+
+			if (!String.IsNullOrWhiteSpace(value) && !String.IsNullOrWhiteSpace(model) && value.ToLower().Trim() != model.ToLower().Trim())
 			{
-				pattern = $"({value.ParseBracket()})|({model.ParseBracket()})";
+				pattern = $"({value})|({model})";
 			}
 			else if (String.IsNullOrWhiteSpace(value))
 			{
-				pattern = model.ParseBracket();
+				pattern = model;
 			}
 			else
 			{
-				pattern = value.ParseBracket();
+				pattern = value;
 			}
 
-			var result = pattern
-				.TrimStart()
-				.Trim()
-				.TrimEnd()
-				.Insert(0, "#")
-				.Insert(pattern.Length + 1, "#")
-				.Replace("-", @"(#|\-)?")
-				.Replace("/", @"(#|\/|\-)?")
-				.Replace(" ", @"(#|\-)?")
-				.Replace("_", @"(#|\_)?")
-				.Replace("&", @"(#|\&)?")
-				.Replace(".", @"(#|\.|\-)?")
-				.Replace(",", @"(#|\,)?")
-				.Replace("+", @"(#|\+|\-)?")
-				.Replace("№", @"(#|\№)?")
-				.Replace("\"", @"(#|\"")?")
-				.Replace("*", @"(#|\*)?");
-			
-			return result;
+
+            #region Main
+            if (String.IsNullOrWhiteSpace(pattern) || pattern == "()|()")
+            {
+                return "";
+            }
+
+            var result = pattern
+                .TrimStart()
+                .Trim()
+                .TrimEnd()
+                .Insert(0, "#")
+                .Insert(pattern.Length + 1, "#")
+                .Replace("-", @"(#|\-)?")
+                .Replace("/", @"(#|\/|\-)?")
+                .Replace(" ", @"(#|\-)?")
+                .Replace("_", @"(#|\_)?")
+                .Replace("&", @"(#|\&)?")
+                .Replace(".", @"(#|\.|\-)?")
+                .Replace(",", @"(#|\,)?")
+                .Replace("+", @"(#|\+|\-)?")
+                .Replace("№", @"(#|\№)?")
+                .Replace("\"", @"(#|\"")?")
+                .Replace("*", @"(#|\*)?")
+                .Replace("®", @"(#|\®)?"); 
+            #endregion
+
+            return result;
 		}
 
-		private static string ParseBracket(this string value)
-		{
-			return value
-				.Replace(")", @"(#|\))?")
-				.Replace("(", @"(#|\()?")
-				.Replace("(#|\\()?#|\\))?", @"(#|\))?");
-		}
-	}
+
+        #region Non-public Methods
+        private static string ParseBracket(this string value)
+        {
+            var charCase = new List<string> { ".", "-", "+", "№", ",", "/", "\"", "*", "(", ")", "®" };
+            var flag = false;
+            foreach (var item in charCase)
+            {
+                if (value.Contains(item))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            value = value
+                .Replace(")", @"(#|\))?")
+                .Replace("(", @"(#|\()?")
+                .Replace("(#|\\()?#|\\))?", @"(#|\))?");
+
+            return flag ? value : "";
+        } 
+        #endregion
+    }
 }
